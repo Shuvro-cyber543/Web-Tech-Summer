9ae7925a0832602cfa3412a0e050f28811c46758
Shuvro-cyber543 <suvra2002@gmail.com>
2025-07-23 14:02:15 +0600

    lab2

---
+++ Mid/HW.php
@@ -0,0 +1,8 @@
+<!Doctype html>
+<html>
+<title> this is a registration form </title>
+<body>
+<center>
+<h1 style = "color:blue;"> aiub registration form </h1>
+<h2 style = "color:green;"> please fill the form </h2>
+</center>
